5615342ab834bbec3594591855db9ccb62fcdaad
SgtPunishment <sgtpunishment@gmail.com>
2015-02-11 22:25:31 +0000

    Update
    Removed non-required files, updated code thanks to Mineshopper, Syndaryl
    and loganwrightoit's help, still not finished

--- src/main/java/roadblock/block/Roadblock.java
+++ src/main/java/roadblock/block/Roadblock.java
@@ -1,17 +1,21 @@
 package roadblock.block;
 
+import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -55,42 +59,66 @@ public class Roadblock extends Block {
 				|| block == Blocks.torch;
 	}
 
+	@Override
+	public int damageDropped(int meta) {
+		return meta;
+	}
+
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@Override
+	public void getSubBlocks(Item item, CreativeTabs creativeTabs, List list) {
+		for (int i = 0; i < 2; i++) {
+
+			list.add(new ItemStack(item, 1, i));
+		}
+	}
+
 	public void setBlockBoundsBasedOnState(IBlockAccess block, int x, int y,
 			int z) {
+		int meta = block.getBlockMetadata(x, y, z);
 		boolean airabove = this.isFullRoad(block, x, y + 1, z);
 		float f4 = 0.9375F;
 
-		if (airabove) {
+		if (airabove && meta == 0) {
 			f4 = 0.4375F;
 		}
 
 		this.setBlockBounds(0.0F, 0.0F, 0.0F, 1.0F, f4, 1.0F);
+		// System.out.println("when do I get here?" + meta);
 	}
 
-	public void onBlockClicked(World world, int xCoord, int yCoord, int zCoord,
-			EntityPlayer player) {
-		// TODO Figure out player right click with item to change block / model
-		// state
+	public void addCollisionBoxesToList(World world, int x, int y, int z,
+			AxisAlignedBB axisAlignedBB, List list, Entity entity) {
+		setBlockBoundsBasedOnState(world, x, y, z);
+		super.addCollisionBoxesToList(world, x, y, z, axisAlignedBB, list,
+				entity);
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, int xCoord, int yCoord,
+			int zCoord, EntityPlayer player, int p_149727_6, float p_149727_7,
+			float p_149727_8, float p_149727_9) {
+
 		if (!world.isRemote) {
 			ItemStack stack = player.getHeldItem();
 			if (stack != null && stack.getItem() == Register.ironMallet) {
-				if (world.getBlock(xCoord, yCoord, zCoord).getBlockBoundsMaxY() == 0.9375F) {
-					world.getBlock(xCoord, yCoord, zCoord).setBlockBounds(0F,
-							0F, 0F, 1F, 0.4375F, 1F);
-					System.out.println("Set to Half");
-					// stack.damageItem(1, player);
+				int meta = world.getBlockMetadata(xCoord, yCoord, zCoord);
+				if (meta == 1) {
+					// System.out.println("Set to Half");
+					world.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 0,
+					2);
+					world.markBlockForUpdate(xCoord, yCoord, zCoord);
+				} else {
+					// System.out.println("Set to Full");
+					world.setBlockMetadataWithNotify(xCoord, yCoord, zCoord, 1,
+							2);
+					world.markBlockForUpdate(xCoord, yCoord, zCoord);
 				}
-
-				if (world.getBlock(xCoord, yCoord, zCoord).getBlockBoundsMaxY() == 0.4375F) {
-					world.getBlock(xCoord, yCoord, zCoord).setBlockBounds(0F,
-							0F, 0F, 1F, 0.9375F, 1F);
-					System.out.println("Set to Full");
-					// stack.damageItem(1, player);
-				}
-			} else {
 
 			}
 		}
+
+		return true;
 	}
 
 	public boolean isOpaqueCube() {
